22cd6c371033f1b9f9717a4c9ca535521fa0f2ff
unknown <comeau@TUCPD047.vpn.mmsi.com>
2014-11-06 13:36:17 -0700

    bla

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 
 #include "p24fj64ga002.h"
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdlib.h>
 #include "p24FJ64GA002.h"
 #include "lcd.h"
+#include <math.h>
 
 // ******************************************************************************************* //
 // Configuration bits for CONFIG1 settings.
@@ -18,8 +18,7 @@
 // These settings are appropriate for debugging the PIC microcontroller. If you need to
 // program the PIC for standalone operation, change the COE_ON option to COE_OFF.
 
-_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
-		 BKBUG_ON & COE_ON & ICS_PGx1 &
+_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & BKBUG_ON & COE_ON & ICS_PGx1 &
 		 FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
 
 // ******************************************************************************************* //
@@ -29,99 +28,67 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
 _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
 		 IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
 
+#define XTFREQ          7372800                     // On-board Crystal frequency
+#define PLLMODE         4                           // On-chip PLL setting (Fosc)
+#define FCY             (XTFREQ*PLLMODE)/2          // Instruction Cycle Frequency (Fosc/2)
 // ******************************************************************************************* //
-// Clock constants
-#define XTFREQ          7372800         	  // On-board Crystal frequency
-#define PLLMODE         4               	  // On-chip PLL setting (Fosc)
-#define FCY             (XTFREQ*PLLMODE)/2    // Instruction Cycle Frequency (Fosc/2)
 
-#define BAUDRATE      115200
-#define BRGVAL        ((FCY/BAUDRATE)/16)-1
-
-// ******************************************************************************************* //
-// Varible used to indicate that the current configuration of the keypad has been changed,
-// and the KeypadScan() function needs to be called.
-volatile int scanKeypad = 0;
-
-// ******************************************************************************************* //
-=======
-#include "lcd.h"
->>>>>>> 1e42594ed8820eafd73fb56b1d2ad6417351403c
+#define PWM_FREQ	100
+#define DUTY_CYCLE	50                          // in percentage
+#define PR_value	(57600/PWM_FREQ)-1          //for prescaler of 1:256
 
 
-<<<<<<< HEAD
-    int ADC_value;
-    char value[8];
-    double AD_value;
-
-    LCDInitialize( );
-
-    AD1PCFGbits.PCFG5;	 	// AN5 input pin is analog
-    AD1CON2 = 0; 		// Configure A/D voltage reference
-    AD1CON3 = 0x0101;
-    AD1CON1 = 0x80E4;
-    AD1CHS = 0; 		// Configure input channels
-    AD1CSSL = 0; 		// No inputs is scanned
-
-    
-
-    return 0;
-}
-=======
-// ******************************************************************************************* //
-// Configuration bits for CONFIG1 settings.
-//
-// Make sure "Configuration Bits set in code." option is checked in MPLAB.
-//
-// These settings are appropriate for debugging the PIC microcontroller. If you need to
-// program the PIC for standalone operation, change the COE_ON option to COE_OFF.
-
-_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
-		 BKBUG_ON & COE_ON & ICS_PGx1 &
-		 FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
-
-// ******************************************************************************************* //
-// Configuration bits for CONFIG2 settings.
-// Make sure "Configuration Bits set in code." option is checked in MPLAB.
-
-_CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
-		 IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
->>>>>>> 1e42594ed8820eafd73fb56b1d2ad6417351403c
-
-#define XTFREQ          7372800         	  // On-board Crystal frequency
-#define PLLMODE         4               	  // On-chip PLL setting (Fosc)
-#define FCY             (XTFREQ*PLLMODE)/2    // Instruction Cycle Frequency (Fosc/2)
-// ******************************************************************************************* //
 int main(void){
 	int ADC_value;
  	char value[8];
 	double AD_value;
+        int OC_value;
+
         TMR3 = 0;
-        PR3 = 50464;           
+        PR3 = PR_value;
         IFS0bits.T3IF = 0;
         IEC0bits.T3IE = 1;
         T3CON = 0x8030;
 
 	LCDInitialize( );
 
-	AD1PCFG &= 0xFFDF;	 	// AN0 input pin is analog
-	AD1CON2 = 0; 		// Configure A/D voltage reference
+	AD1PCFG &= 0xFFDF;                      // AN5 input pin is analog
+	AD1CON2 = 0;                            // Configure A/D voltage reference
 	AD1CON3 = 0x0101;
 	AD1CON1 = 0x20E4;
-	AD1CHS = 5; 		// Configure input channels
-	AD1CSSL = 5; 		// No inputs is scanned
-	IFS0bits.AD1IF = 0; // Clear A/D conversion interrupt.
-	AD1CON1bits.ADON = 1; // Turn on A/D
+	AD1CHS = 5;                             // Configure input channels
+	AD1CSSL = 5;                            // No inputs is scanned
+	IFS0bits.AD1IF = 0;                     // Clear A/D conversion interrupt.
+	AD1CON1bits.ADON = 1;                   // Turn on A/D
+
+        TRISBbits.TRISB8 = 0;
+	RPOR4bits.RP8R = 18;                    //pin RB8 (RP8) for OC1
+        TRISBbits.TRISB9 = 0;
+        RPOR4bits.RP9R = 19;                    //pin RB9 (RP9) for OC2
+        OC_value = DUTY_CYCLE*(PR_value/100);   // if using this, Make sure that overflow does not occur
+	OC1R = OC_value;                        //initial duty cycle for OC1
+        OC2R = (1-OC_value);                    //initial duty cycle for OC2
+        OC1RS = OC_value;
+        OC2RS = (1-OC_value);
+        OC1CON = 0x000E;
+        OC2CON = 0x000E;
+
 	while(1){
 		while (IFS0bits.AD1IF ==0){};     // AD1CON1bits.DONE can be checked instead
 		IFS0bits.AD1IF = 0;
 		ADC_value = ADC1BUF0;
 
 		sprintf(value, "%6d", 1023 - ADC_value);
-		LCDMoveCursor(0,0); LCDPrintString(value);
-//		AD_value = (ADC_value * 3.3)/1024;
-//		sprintf(value, "%6.2f", AD_value);
-//		LCDMoveCursor(1,0); LCDPrintString(value);
+		//LCDMoveCursor(0,0);
+                //LCDPrintString(value);
+		AD_value = (ADC_value * 3.3)/1024;
+                OC_value = (int)((AD_value / 3.3) * 100) * (PR_value/100);
+		sprintf(value, "%6.2f", AD_value);
+		//LCDMoveCursor(1,0);
+                //LCDPrintString(OC_value);
+
+                printf("%d\n" OC_value);
+
 	}
 	return 0;
 }
